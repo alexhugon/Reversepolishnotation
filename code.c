e357690407cb9c151b37859d8f2b3573f79c0513
alexhugon <43140888+alexhugon@users.noreply.github.com>
2019-10-30 17:02:41 +0100

    Update code.c

--- code.c
+++ code.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 #define TAILLEINPUT 11
+int err =0;
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -23,16 +24,28 @@ pile* pilesuivant(int valeur, pile* precedent) /* Ajoute un niveau au dessus de 
         exit(1);
     }
     suivant->valeur = valeur;
-    suivant->precedent = precedent;
+    suivant->suivant = suivant;
     return suivant;
 }
 
-void afficherpile(pile* niveau) /* Affiche les valeurs des différents niveau de la pile, en partant du BAS vers le HAUT */
+pile* ajoutpile(pile* comm, pile* niveau)
 {
-    if ( level != NULL ) /* Plus de niveau inférieur, on a fini de parcourir la pile */
+    if ( comm == NULL ) /* Aucune pile, deviens la pile */
     {
-        afficherpile(niveau->suivant);
-        printf("%d", niveau->valeur); /* du bas vers le haut, à passer au dessus de l'instruction pile_show(level->under); pour inverser */
+        niveau->suivant = NULL;
+        return niveau;
+    }
+    niveau->suivant = niveau;
+    return niveau;
+}
+
+void afficherpile(pile* niveau, int etage) /* Affiche les valeurs des différents niveau de la pile */
+{
+    if ( niveau != NULL ) 
+    {
+        afficherpile(niveau->suivant, etage + 1);
+        printf("%d", niveau->valeur);
+        if ( etage != 0){printf(" ");}
     }
 }
 int intoupas (char instruction[TAILLEINPUT])
@@ -46,16 +59,78 @@ int intoupas (char instruction[TAILLEINPUT])
 int transformint (char instruction[TAILLEINPUT])
 {
     char *ptr;
-    return strtol(instruction, &ptr, CONVERTION_BASE);
+    return strtol(instruction, &ptr, 10);
 }
+pile* divi( pile* niveau )
+{
+    return niveau;
+}
+pile* mul( pile* niveau )
+{
+    return niveau;
+}
+
+pile* mod( pile* niveau )
+{
+    return niveau;
+}
+
+pile* pop( pile* niveau )
+{
+    return niveau;
+}
+pile* rol( pile* prec, pile* niveau, int o)
+{
+    return niveau;
+}
+pile* swp( pile* niveau )
+{
+    return niveau;
+}
+
+pile* sub( pile* niveau ) /* soustraction */
+{
+    if ( niveau == NULL ) 
+        return NULL;
+    if ( niveau->suivant == NULL )
+    {
+        err = 1;
+        niveau = pop(niveau);
+        return niveau;
+    }
+    int gauche = niveau->suivant->valeur, droit = niveau->valeur;
+    pile* suivant = niveau->suivant->suivant;
+    free(niveau->suivant);
+    free(niveau);
+    return ajoutpile(suivant, pilesuivant(gauche-droit, suivant));
+}
+
+pile* add( pile* niveau ) /* addition */
+{
+    if ( niveau == NULL )
+        return NULL;
+    if ( niveau->suivant == NULL )
+    {
+        err = 1;
+        niveau = pop(niveau);
+        return niveau;
+    }
+    int gauche = niveau->suivant->valeur, droit = niveau->valeur;
+    pile* suivant = niveau->suivant->suivant;
+    free(niveau);
+    return ajoutpile(suivant, pilesuivant(gauche+droit, suivant));
+}
+
+
+
 pile* operation ( pile* commande, char instruction[TAILLEINPUT] )
 {
     if ( !strcmp("ROL", instruction) )
     {
-        int n = commande->value;
-        commande = commande_pop(commande);
-        n = pile_rol(commande, commande->under, n-1);
-        commande = level_add(commande, pilesuivant(n, commande));
+        int n = commande->valeur;
+        commande = pop(commande);
+        n = rol(commande, commande->suivant, n-1);
+        commande = ajoutpile(commande, pilesuivant(n, commande));
     }
     else if ( !strcmp("SWP", instruction) )
         commande = swp(commande);
@@ -66,7 +141,7 @@ pile* operation ( pile* commande, char instruction[TAILLEINPUT] )
     else if ( !strcmp("MOD", instruction) )
         commande = mod(commande);
     else if ( !strcmp("DIV", instruction) )
-        commande = div(commande);
+        commande = divi(commande);
     else if ( !strcmp("MUL", instruction) )
         commande = mul(commande);
     else if ( !strcmp("SUB", instruction) )
@@ -83,22 +158,19 @@ int main()
     
     int N;
     scanf("%d", &N);
-    pile commande = NULL;
+    pile* commande = NULL;
     for (int i = 0; i < N; i++) {
         char instruction[TAILLEINPUT];
         scanf("%s", instruction);
         if ( intoupas(instruction) )
-            commande = pilesuivant(transformint(instruction), commande);
+            commande = ajoutpile(commande, pilesuivant(transformint(instruction), commande));
         else
             commande = operation(commande, instruction);
         
         
     }
-
-    // Write an action using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    printf("answer\n");
+    afficherpile(commande,0);
+    printf("\n");
 
     return 0;
 }
